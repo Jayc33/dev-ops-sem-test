96127e1b8ad2ebd189fcfd7b547196d856e6160a
claajanu <jan.claar@student.uni-augsburg.de>
2021-07-23 22:53:38 +0200

    change: btn text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,9 +34,9 @@
 
                 
                 <div class="d-flex justify-content-between mt-5" style="width: 500px">
-                    <a class="btn btn-outline-light" href="/worksfast">This might take a while...</a>
+                    <a class="btn btn-outline-light" href="/worksfast">Takes no time at all!</a>
                     <a class="btn btn-outline-light" href="/noproblem">No Prob</a>
-                    <a class="btn btn-outline-light" href="/works">Surely this will work...</a>
+                    <a class="btn btn-outline-light" href="/definitelyworks">Surely this will work...</a>
                 </div>
             </div>
         </div>
